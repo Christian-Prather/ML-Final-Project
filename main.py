2fee06b6a4b4546c7859b82a215f085d2423d234
Christian <christianprather@smilingrobotics.com>
2020-11-01 19:16:52 -0700

    Added base file

---
+++ main.py
@@ -0,0 +1,38 @@
+
+import math
+import time
+# import cv2
+import numpy as np
+import pyrealsense2 as rs
+
+
+# Configure depth and color streams
+pipeline = rs.pipeline()
+config = rs.config()
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+# Start streaming
+pipeline.start(config)
+points = rs.points
+frames = pipeline.wait_for_frames()
+depth = frames.get_depth_frame()
+
+# Get stream profile and camera intrinsics
+# profile = pipeline.get_active_profile()
+# depth_profile = rs.video_stream_profile(profile.get_stream(rs.stream.depth))
+# depth_intrinsics = depth_profile.get_intrinsics()
+# w, h = depth_intrinsics.width, depth_intrinsics.height
+
+# Processing blocks
+pc = rs.pointcloud()
+# decimate = rs.decimation_filter()
+# decimate.set_option(rs.option.filter_magnitude, 2 ** state.decimate)
+# colorizer = rs.colorizer()
+
+points = pc.calculate(depth)
+depth_image = np.asanyarray(depth.get_data())
+vertecies = points.get_vertecies()
+
+print("////////////////////////////////////////////////////////////////////////////")
+print(vertecies)
