4a3bc56cbe07d3c87639ef177dfac3f90819649c
Christian Prather <christian@luci.com>
2020-11-01 21:36:52 -0700

    Saving sparse files

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import time
 # import cv2
 import numpy as np
 import pyrealsense2 as rs
-
+import cv2
+import pandas as pd
 
 # Configure depth and color streams
 pipeline = rs.pipeline()
@@ -27,17 +28,35 @@ frames = pipeline.wait_for_frames()
 pc = rs.pointcloud()
 decimate = rs.decimation_filter()
 decimate.set_option(rs.option.filter_magnitude, 2 ** 2)
-# colorizer = rs.colorizer()
-depth = frames.get_depth_frame()
-depth = decimate.process(depth)
+colorizer = rs.colorizer()
+file_name = 0
+while True:
+    userInput = input("Press key to take snapshot....")    
+    if userInput == "z":
+        exit
+    depth = frames.get_depth_frame()
+    color_frame = frames.get_color_frame()
+    depth = decimate.process(depth)
 
-points = pc.calculate(depth)
-depth_image = np.asanyarray(depth.get_data())
-vertecies = points.get_vertices()
-verts = np.asanyarray(vertecies)
-print("////////////////////////////////////////////////////////////////////////////")
-# for vert in verts:
-#     print(vert)
-print(verts.shape)
+    points = pc.calculate(depth)
+    depth_image = np.asanyarray(depth.get_data())
+    color_image = np.asanyarray(color_frame.get_data())
+    depth_colormap = np.asanyarray(colorizer.colorize(depth).get_data())
+    vertecies = points.get_vertices()
+    verts = np.asanyarray(vertecies)
+    print("////////////////////////////////////////////////////////////////////////////")
+    # for vert in verts:
+    #     print(vert)
+    print(verts.shape)
+    # cv2.imshow("Image", depth_colormap)
+    # cv2.waitKey(10)
+    # data_frame = pd.DataFrame(data=verts, index = ['Row_' + str(i + 1)  
+    #                     for i in range(verts.shape[0])],columns=["x", "y", "z"])
+    # print(data_frame)
+    # drop_series = (data_frame != 0).any(axis=1)
+    # filtered_data = data_frame.loc[drop_series]
 
-np.savetxt('data.csv', verts, delimiter=",")
+    # filtered_data.to_csv('data' + str(file_name))
+    np.savetxt('data' + str(file_name) + '.csv', verts, delimiter=",")
+    cv2.imwrite('data' + str(file_name)+ '.jpg', depth_colormap)
+    file_name+=1
